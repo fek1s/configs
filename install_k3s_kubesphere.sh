9a0b99314560490f6193171fce2891a36178d0c7
fek1s <114650891+fek1s@users.noreply.github.com>
2024-07-29 19:55:35 +0200

    Update install_k3s_kubesphere.sh

--- install_k3s_kubesphere.sh
+++ install_k3s_kubesphere.sh
@@ -39,6 +39,6 @@ kubectl apply -f cluster-configuration.yaml
 
 echo "K3s and Kubesphere installation complete."
 
-sleep 60 
+# sleep 60 
 
-kubectl logs -n kubesphere-system $(kubectl get pod -n kubesphere-system -l 'app in (ks-install, ks-installer)' -o jsonpath='{.items[0].metadata.name}') -f
+# kubectl logs -n kubesphere-system $(kubectl get pod -n kubesphere-system -l 'app in (ks-install, ks-installer)' -o jsonpath='{.items[0].metadata.name}') -f
